72f35315625b12297c551e5863283898893191c7
webend-uk <devops@webend.uk>
2024-04-15 16:45:42 +0100

    add func

--- ct-webend/node-red.sh
+++ ct-webend/node-red.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
-# License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
+source <(curl -s https://raw.githubusercontent.com/webend-uk-forks/tteck-proxmox/webend-homelab/misc-webend/build.func)
 
 function header_info {
 clear
