9ab56f04d6c944b505b367c722ca0b5efb1d95e4
WebEnd <devops@webend.uk>
2024-05-20 04:09:58 +0100

    add needed folders

--- install-webend/docker3-pve-install.sh
+++ install-webend/docker3-pve-install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# prepare destinations
+mkdir -p /b_apps/pve3-docker
+chmod -R a+w /b_apps/
+
 LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
 cat <<EOF >>$LXC_CONFIG
 mp0: /b_apps/pve3-docker,mp=/b_apps
