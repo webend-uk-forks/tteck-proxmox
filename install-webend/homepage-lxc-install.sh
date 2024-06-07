800f50a32d75e812d5484818d361011a91477d0d
WebEnd <devops@webend.uk>
2024-06-07 05:25:56 +0100

    add pub key

--- install-webend/homepage-lxc-install.sh
+++ install-webend/homepage-lxc-install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo "Add custom keys"
-
+touch /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/LziN55YNs0IfQXr7uksDxhBvy12nJpA92EL6j+1/n webend-console-homelab-211-macbookair" >> /root/.ssh/authorized_keys
